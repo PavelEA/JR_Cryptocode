3b14a85afa2d0ebc3b80b5e7ff2e7379f33939e6
Pavel <erovenko.pavel@yandex.ru>
2022-03-15 13:10:43 +0300

    Создан метод setCaesarCiepher для шифрования символов

--- scr/java/com/javarush/cryptoanalyser/Main.java
+++ scr/java/com/javarush/cryptoanalyser/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import static java.lang.String.valueOf;
 
 public class Main {
+    final int smeshenie = 2;
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
@@ -16,55 +17,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String abc = "./scr/java/com/javarush/cryptoanalyser/input.txt";
         String fileName = scanner.nextLine();
-        /*try (RandomAccessFile randomAccessFile = new RandomAccessFile("./scr/java/com/javarush/cryptoanalyser/input.txt", "rw");
-             FileChannel fileChannel = randomAccessFile.getChannel()) {
-
-            ByteBuffer byteBuffer = ByteBuffer.allocate(100);
-
-            StringBuilder stringBuilder = new StringBuilder();
-            fileChannel.read(byteBuffer);
-
-            while (byteBuffer.hasRemaining()) {
-                stringBuilder.append((char) byteBuffer.get());
-            }
-            System.out.println("Здесь будет текст из файла: ");
-            String sometext = "ты это, заходи если че";
-            System.out.println(stringBuilder + sometext);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-       /* try (BufferedWriter bufferedWritter = new BufferedWriter(new FileWriter("file.txt"));
-             BufferedReader bufferedReader = new BufferedReader(new FileReader("./scr/java/com/javarush/cryptoanalyser/input.txt"))) {
-            while (bufferedReader.ready()) {
-                char line = (char) bufferedReader.read();
-                System.out.println(line);
-                System.out.println("-----------------------");
-
-
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 
         System.out.println(ReadFiles(abc));
-        chezar(ReadFiles(abc));
+
 
 
     }
 
 
-    public static char chezar(ArrayList<String> textFile) {
-        final int smeshenie = 2;
-        for (int i = 0; i < ALPHABET.length; i++) {
-            char abc = ALPHABET[i];
-            if (valueOf(abc).equals(valueOf(textFile))) {
-                ALPHABET[i] = ALPHABET[i + smeshenie];
-            }
+    public static ArrayList<Character> setCaesarCipher(ArrayList<String> textFile,int smeshenie) {
+        ArrayList<Character> newTextFile = new ArrayList<>();
+        for (int i = 0; i < textFile.size(); i++) {
+            if (textFile.get(i).equals(valueOf(ALPHABET[i])))
+            newTextFile.add(i,ALPHABET[i+smeshenie]);
         }
-        return 0;
+        return newTextFile;
     }
 
 
@@ -79,5 +46,9 @@ public class Main {
             e.printStackTrace();
         }
         return textFile;
+    }
+
+    public void fileExist(String file){
+
     }
 }
