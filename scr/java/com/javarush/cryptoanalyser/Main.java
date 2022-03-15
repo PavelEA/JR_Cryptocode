e2749ac7dd808fbc9c5f0d8c8cd64d99e37eb9eb
Pavel <erovenko.pavel@yandex.ru>
2022-03-15 17:03:41 +0300

    Закончен метод readFiles.

--- scr/java/com/javarush/cryptoanalyser/Main.java
+++ scr/java/com/javarush/cryptoanalyser/Main.java
@@ -1,54 +1,64 @@
 package com.javarush.cryptoanalyser;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 import static java.lang.String.valueOf;
 
 public class Main {
-    final int smeshenie = 2;
+    static String abc = "./scr/java/com/javarush/cryptoanalyser/input.txt";
+    static final int smeshenie = 2;
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
-            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'',':', '!', '?',' ' };
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String abc = "./scr/java/com/javarush/cryptoanalyser/input.txt";
-        String fileName = scanner.nextLine();
 
-        System.out.println(ReadFiles(abc));
+
+
+
+        System.out.println();
 
 
 
     }
 
 
-    public static ArrayList<Character> setCaesarCipher(ArrayList<String> textFile,int smeshenie) {
-        ArrayList<Character> newTextFile = new ArrayList<>();
+    public static ArrayList<Character> setCaesarCipher(ArrayList<Character> textFile, int offset) {
+        ArrayList<Character> fileToWrite = new ArrayList<>(textFile);
         for (int i = 0; i < textFile.size(); i++) {
-            if (textFile.get(i).equals(valueOf(ALPHABET[i])))
-            newTextFile.add(i,ALPHABET[i+smeshenie]);
+            char a = textFile.get(i);
+            for (int j = 0; j < ALPHABET.length ; j++) {
+                if(a == ALPHABET[j]){
+                    fileToWrite.set(i, ALPHABET[j + offset]);
+                }
+            }
+
         }
-        return newTextFile;
+        return fileToWrite;
     }
 
 
-    public static ArrayList<String> ReadFiles(String file) {
-        ArrayList<String> textFile = new ArrayList<>();
+    public static ArrayList<Character> readFiles(String file) {
+        ArrayList<Character> textFile = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-            while (bufferedReader.ready()) {
-                textFile.add(bufferedReader.readLine());
+            while(bufferedReader.ready()){
+                char [] chars = bufferedReader.readLine().toCharArray();
+                for (char c : chars) {
+                    textFile.add(c);
+                }
             }
-            return textFile;
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println(textFile);
         return textFile;
-    }
-
-    public void fileExist(String file){
 
     }
+
+
+
+
 }
