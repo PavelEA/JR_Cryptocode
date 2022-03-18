6bb34a3303f3c32088d7b7a90e516d74abacb37e
PavelEA <87639661+pavelea@users.noreply.github.com>
2022-03-18 19:03:58 +0300

    Классы ForTheUser,FileOptions,CaesarEncryption доработаны, работают в соответствии с заданием. Помечены комментарии в местах где необходима доработка.
    Signed-off-by: Pavel <erovenko.pavel@yandex.ru>

--- scr/java/com/javarush/cryptoanalyser/FileOptions.java
+++ scr/java/com/javarush/cryptoanalyser/FileOptions.java
@@ -8,8 +8,9 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class FileOptions {
-    static String abc = ForTheUser.getFilePath();//"./scr/java/com/javarush/cryptoanalyser/input.txt";
-    private static final String FILE_NOT_FOUND_EXEPTION = " Файл указаному адресу не существует или вы ввели не корректный адрес";
+    //Пример файл оригинальный: ./scr/java/com/javarush/cryptoanalyser/input.txt
+    //Пример файл для записи: C:\Users\Pashok\IdeaProjects\JavaRush_Crypto\scr\java\com\javarush\cryptoanalyser\someText.txt
+    private static final String FILE_NOT_FOUND_EXEPTION = " 1111111 Файл указаному адресу не существует или вы ввели не корректный адрес";
     private static final String OUTPUT_FILE_NOT_FOUND = "Файл для записи не существует. Попробуйте еще раз";
 
     public static ArrayList<Character> readFiles(String file) {
@@ -21,25 +22,20 @@ public class FileOptions {
                     textFile.add(c);
                 }
             }
-
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println(FILE_NOT_FOUND_EXEPTION);
-            CaesarEncryption.dataCollection();
-
-
+            CaesarEncryption.getOriginalFileLocationAddress();
         }
-        System.out.println(textFile);
         return textFile;
-
     }
 
-    public static boolean fileExist(Path fileName) {
-        return Files.exists(fileName);
+    public static void writeFiles(ArrayList<Character> textForWrite, String addressForWrite) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(addressForWrite))) {
+            for (char c : textForWrite) {
+                bufferedWriter.write(c);
+            }
+        } catch (Exception e) {
+            System.out.println(OUTPUT_FILE_NOT_FOUND);
+        }
     }
-
-    public static boolean equals(String a, String b){
-        return a.equals(b);
-    }
-
 }
--- scr/java/com/javarush/cryptoanalyser/Main.java
+++ scr/java/com/javarush/cryptoanalyser/Main.java
@@ -5,18 +5,5 @@ package com.javarush.cryptoanalyser;
 public class Main {
        public static void main(String[] args) {
            ForTheUser.hello(ForTheUser.getWelcomePart(),ForTheUser.getTypesOfCiphers(),ForTheUser.getCHOICE());
-
-
-
        }
-
-
-
-
-
-
-
-
-
-
 }
