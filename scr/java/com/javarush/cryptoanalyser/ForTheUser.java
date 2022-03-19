3d82645c6492fb08203a796ae18de240836d09ba
PavelEA <87639661+pavelea@users.noreply.github.com>
2022-03-19 08:42:09 +0300

    Добавлен выход из приложения в меню ForTheUser
    Signed-off-by: Pavel <erovenko.pavel@yandex.ru>

--- scr/java/com/javarush/cryptoanalyser/ForTheUser.java
+++ scr/java/com/javarush/cryptoanalyser/ForTheUser.java
@@ -8,14 +8,15 @@ public class ForTheUser {
     private static final String TYPES_OF_CIPHERS = " 1.Шифровка текста по стилю Цезаря \n 2.Расшифровка текста по стилю Цезаря \n 3.Расшифровка текста с помощью brute force \n " +
             "4.Расшифровка с помощью статистического анализа текста \n";
     private static final String CHOICE = "Выберите необходимый режим, нажатием соответствующего числа";
-    private static final String EXIT = "\n Для выхода из программы напишите exit";
-    private static final String MODE_NOT_FOUND = "Такого режима не существует, введите число от 1 до 4";
+    private static final String EXIT = "\n Для выхода из программы нажмите 0";
+    private static final String MODE_EXIT = "\n Вы вышли из приложения. До свидания";
+    private static final String MODE_NOT_FOUND = "Такого режима не существует, введите число от 1 до 4, для выхода 0";
     private static final String MODE_ONE = "Вы выбрали режим 1. Шифровка текста по стилю Цезаря. Для этого необходимо ввести адрес оригинального файла, ключ и адрес файла для записи шифра ";
     private static final String MODE_SECOND = " Вы выбрали режим 2.Расшифровка текста по стилю Цезаря. Для этого необходимо ввести сдвиг по алфавиту который использовался при шифровании (ключ)," +
             " адрес куда писать расшифрованный файл и адрес к зашифрованному файлу";
     private static final String MODE_THIRD = " Вы выбрали режим 3жажажаж. Для этого необходимо ввести ключ и путь к файлу";
     private static final String MODE_FOURTH = " Вы выбрали режим 4жажажаж. Для этого необходимо ввести ключ и путь к файлу";
-    private static final String SUCCSESS = "Шифрование успешно завершено. Проверьте свой файл.";
+    private static final String SUCCESS = "Шифрование успешно завершено. Проверьте свой файл.";
 
 
     public static String getWelcomePart() {
@@ -31,12 +32,12 @@ public class ForTheUser {
     public static String getEXIT() { return EXIT; }
 
     public static void hello(String welcomePart, String typesOfCiphers, String choice) {
-        System.out.println(welcomePart);
-        System.out.println(typesOfCiphers);
+        System.out.println(WELCOME_PART);
+        System.out.println(getTypesOfCiphers());
         System.out.println(choice);
         System.out.println(EXIT);
         startMainMenu();
-    }//Тестовый коммит
+    }
 
     public static void startMainMenu()//нужен выход из программы
     {
@@ -47,22 +48,25 @@ public class ForTheUser {
                 case 1:
                     System.out.println(MODE_ONE);
                     CaesarEncryption.doCaesarCipher();
-                    System.out.println(SUCCSESS);
+                    System.out.println(SUCCESS);
                     break;
                 case 2:
                     System.out.println(MODE_SECOND);
                     //вызов метода для 2 задания
-                    System.out.println(SUCCSESS);
+                    System.out.println(SUCCESS);
                     break;
                 case 3:
                     System.out.println(MODE_THIRD);
                     // вызов метода для 3 задания
-                    System.out.println(SUCCSESS);
+                    System.out.println(SUCCESS);
                     break;
                 case 4:
                     System.out.println(MODE_FOURTH);
                     // вызов метода для 4 задания
-                    System.out.println(SUCCSESS);
+                    System.out.println(SUCCESS);
+                    break;
+                case 0:
+                    System.out.println(MODE_EXIT);
                     break;
                 default:
                     System.out.println(MODE_NOT_FOUND);
